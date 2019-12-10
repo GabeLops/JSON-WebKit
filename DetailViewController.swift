a9bde96892c17cab5c2b665b40a72d7d4cf47f0e
GabeLops <gabrieldlops@gmail.com>
2019-12-09 18:12:46 -0800

    Almost done

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -7,11 +7,35 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailedItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let detailItem = detailedItem else {return}
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
 
         // Do any additional setup after loading the view.
     }
